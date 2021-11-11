23c89b4840abd22635527e91bb99f1e3305c0e0c
Ali Kanıberk <thtramboreis1@gmail.com>
2021-11-11 20:34:15 +0300

    Proje dosyası ekle.

---
+++ Hatırlayıcı/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿
+namespace Hatırlayıcı
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.gettext = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(925, 41);
+            this.panel1.TabIndex = 0;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(48, 41);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.FlatAppearance.BorderSize = 3;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(879, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(46, 41);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "X";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(54, 2);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(177, 39);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Hatırlayıcı";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 38;
+            this.listBox1.Location = new System.Drawing.Point(15, 44);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(882, 422);
+            this.listBox1.TabIndex = 2;
+            this.listBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.listBox1_MouseUp);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.listBox1);
+            this.groupBox1.Location = new System.Drawing.Point(10, 47);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(903, 476);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Kopyalanmış Metinler";
+            // 
+            // gettext
+            // 
+            this.gettext.Enabled = true;
+            this.gettext.Interval = 1000;
+            this.gettext.Tick += new System.EventHandler(this.gettext_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(306, 512);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(346, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "@kaniberkali tarafından yazılmıştır.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(20F, 38F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.ClientSize = new System.Drawing.Size(925, 542);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(11, 9, 11, 9);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Hatırlayıcı";
+            this.TopMost = true;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Timer gettext;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Hatırlayıcı/Form1.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Data.OleDb;
+using System.Runtime.InteropServices;
+using System.IO;
+using Microsoft.Win32;
+
+namespace Hatırlayıcı
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            CheckForIllegalCrossThreadCalls = false;
+            InitializeComponent();
+        }
+
+        OleDbConnection baglanti = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=veritabani.accdb");
+        OleDbCommand komut = new OleDbCommand();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                baglanti.Open();
+                baglanti.Close();
+                Thread klavyethread = new Thread(klavyeoku); klavyethread.Start();
+                Thread kopyahread = new Thread(kopyaoku); kopyahread.Start();
+                verilerigetir();
+                try
+                {
+                    RegistryKey uygulama = Registry.CurrentUser.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\Run", true);
+                    uygulama.SetValue("Hatirlayıcı", "\"" + Application.ExecutablePath + "\"");
+                }
+                catch { }
+            }
+            catch 
+            {
+                if (File.Exists("veritabani.accdb") == false)
+                {
+                    MessageBox.Show("Lütfen programın bulunduğu klasöre veritabanini atın. Eğer veritabanını bulamıyorsanız programı tekrar indirin.", "@kodzamani.tk");
+                    Application.Exit();
+                }
+            }
+        }
+
+        [DllImport("User32.dll")]
+        public static extern int GetAsyncKeyState(Int32 i);
+        private void klavyeoku()
+        {
+            for (; ; )
+            {
+                Thread.Sleep(50);
+                if (this.Visible == false)
+                {
+                    int ctrl = GetAsyncKeyState(16);
+                    int shift = GetAsyncKeyState(17);
+                    int c = GetAsyncKeyState(67);
+                    if (ctrl != 0 && shift != 0 && c != 0)
+                        this.Show();
+                }
+            }
+        }
+        string gelenkopya = "";
+        private void verilerigetir()
+        {
+            try
+            {
+                baglanti.Open();
+                OleDbDataReader reader = null;
+                komut = new OleDbCommand("select* from metinler", baglanti);
+                reader = komut.ExecuteReader();
+                while (reader.Read())
+                {
+                    listBox1.Items.Add(reader["Metin"].ToString());
+                }
+                baglanti.Close();
+            }
+            catch { }
+        }
+        private void kopyaoku()
+        {
+            for (; ; )
+            {
+                
+                if (kopya!=""&&kopya != gelenkopya&& listBox1.Items.Contains(kopya)==false)
+                {
+                    gelenkopya = kopya;
+                    try
+                    {
+                        komut = new OleDbCommand("INSERT into metinler (Tarih, Metin) Values('" + DateTime.Now + "','" + gelenkopya + "')");
+                        komut.Connection = baglanti;
+                        baglanti.Open();
+                        komut.ExecuteNonQuery();
+                        baglanti.Close();
+                        listBox1.Items.Insert(0, gelenkopya);
+                    }
+                    catch { }
+                }
+                Thread.Sleep(1000);
+            }
+        }
+        string kopya = "";
+        private void gettext_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                kopya = Clipboard.GetText();
+            }
+            catch { }
+        }
+
+
+        private void listBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                try
+                {
+                    if (listBox1.Text != ""&&listBox1.SelectedIndex!=-1)
+                    {
+                        Clipboard.SetText(listBox1.Text);
+                    }
+                }
+                catch { }
+            }
+            if (e.Button == MouseButtons.Right)
+            {
+                try
+                {
+                    string sil = listBox1.Text;
+                    if (sil != "")
+                    {
+                        Clipboard.SetText("https://kodzamani.weebly.com");
+                        kopya = "https://kodzamani.weebly.com";
+                        baglanti.Open();
+                        komut.Connection = baglanti;
+                        komut.CommandText = "delete from metinler where Metin='" + sil + "'";
+                        komut.ExecuteNonQuery();
+                        baglanti.Close();
+                        listBox1.SelectedIndex = -1;
+                        listBox1.Items.Remove(sil);
+                    }
+                }
+                catch
+                {
+                   
+                }
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            e.Cancel = true;
+            this.Hide();
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            Move_Z = 1;
+            Mouse_X = e.X;
+            Mouse_Y = e.Y;
+        }
+
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            Move_Z = 0;
+        }
+        int Move_Z;
+        int Mouse_X;
+        int Mouse_Y;
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (Move_Z == 1)
+            {
+                this.SetDesktopLocation(MousePosition.X - Mouse_X, MousePosition.Y - Mouse_Y);
+            }
+        }
+    }
+}
